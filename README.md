0b6b1a9f2650a497392920db8b42b167633b2cc3
Dylan Bunch <baalnaroz@tutanota.com>
2023-01-20 22:55:08 -0600

    adding install script, need to test if it works

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # dotfiles
 New and improved dotfiles repository since the old one was horribly ran and out-of-date.
+
+### Vim
+I use neovim. Once installed, run `:PlugInstall` inside the editor to install plugins.
---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# link .vim folder and .vimrc
+# ln -s ./vim/.vim ~/.vim
+# ln -s ./vim/.vimrc ~/.vimrc
+
+# copy vim theme into .vim/plugs/.../vim-airline-themes once :PlugInstall has been executed
+
+# link .config/nvim folder(s)
+# ln -s ./nvim/nvim ~/.config/nvim
+
+# link ~ (bash) folder(s)
+# link ~ (X) folder(s)
